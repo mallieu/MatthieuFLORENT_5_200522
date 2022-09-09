85f46cd3293d8230aef6be8291c2bad311435a4c
Mallieu <mallieu@hotmail.fr>
2022-09-09 14:48:32 +0200

    Mise en place de la page panier

--- front/js/modules/cart.js
+++ front/js/modules/cart.js
@@ -1,15 +1,19 @@
+const sectionProduitsPanier = document.getElementById("cart__items");
+
 affichagePanier();
 interactionPanier();
 
 // Fonction pour générer un bloc HTML par produit
-async function affichagePanier() {
-  let html = '';
-  const sectionProduitsPanier = document.getElementById("cart__items");
-  panier = JSON.parse(localStorage.getItem("Cart"));
-
-  // Pour chaque objet dans le panier
-  panier.forEach((produitsPanier) => {
-      html += `
+function affichagePanier() {
+    let html = "";
+    panier = JSON.parse(localStorage.getItem("Cart")) || []; // Récupération du panier
+    if (panier.length === 0) { // Vérifie l"existence du panier
+        html = "";
+        sectionProduitsPanier.innerHTML = html;
+        console.log("Le panier est vide. Merci d\'ajouter un produit");
+    } else {
+        panier.forEach((produitsPanier) => { // Génération des produits à partir du panier
+            html += `
       <article class="cart__item" data-id="${produitsPanier._id}" data-color="${produitsPanier.color}">
         <div class="cart__item__img">
           <img src="${produitsPanier.imageUrl}" alt="${produitsPanier.altTxt}">
@@ -30,74 +34,62 @@ async function affichagePanier() {
             </div>
           </div>
         </div>
-      </article>`;     
-      sectionProduitsPanier.innerHTML = html;
-  });
+      </article>`;
+            sectionProduitsPanier.innerHTML = html;
+        });
+        interactionPanier()
+        calculPrixTotal()
+        calculQuantiteTotal()
+    }
 }
 
-async function interactionPanier() {
-  await affichagePanier()
-  document.querySelectorAll('.deleteItem').forEach(settingTrigger => {
-    settingTrigger.addEventListener('click', suppressionProduitPanier)
-  })
-}
-	// Ajout panier
-function suppressionProduitPanier() {
-  panier = JSON.parse(localStorage.getItem("Cart"));
-  panier.pop(this);
-  localStorage.setItem("Cart", JSON.stringify(panier)); 
+// Ajout des triggers sur chaque produit
+function interactionPanier() {
+    document.querySelectorAll(".deleteItem").forEach((settingTrigger) => {
+        settingTrigger.addEventListener("click", suppressionProduitPanier)
+    })
 }
 
-// ON CLICK CLASS DELETEITEM
+// Suppression panier
+function suppressionProduitPanier(trigger) {
+    // Isolement du bloc HTML du produit à partir du bouton cliqué
+    const clickedButton = trigger.target.closest(".cart__item");
 
-// // 1) Convertir 
-// // 2) Lister 
-// // 3) Insérer les blocs HTML 
-// // Insertion d'un bloc HTML complet par canapé depuis le panier en localStorage
-// function importPageAccueil(data, html) {
-//     for (let products of localS) {
-//     }
-//     return html;
-// }
+    // Récupération des attributs pour modifications du panier
+    const produitPanierId = clickedButton.dataset.id;
+    const produitPanierColor = clickedButton.dataset.color;
 
-// /*4) Faire la somme 
+    // Correspondance du produit de la page avec le produit du panier
+    let thisKanap = panier.find((ProduitPanier) => ProduitPanier._id === produitPanierId) && panier.find((ProduitPanier) => ProduitPanier.color === produitPanierColor);
 
-// 5) Agir sur les items pour modifier le panier 
-// 6) Penser au passage à 0 qui supprime l'item 
-// 7) Transition avec le bouton pour passer commande
+    // Récupère l"index du produit
+    let productIndex = panier.indexOf(thisKanap);
 
-//             <section id="cart__items"> => Ajout après ça
-//              <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-//                 <div class="cart__item__img">
-//                   <img src="../images/product01.jpg" alt="Photographie d'un canapé">
-//                 </div>
-//                 <div class="cart__item__content">
-//                   <div class="cart__item__content__description">
-//                     <h2>Nom du produit</h2>
-//                     <p>Vert</p>
-//                     <p>42,00 €</p>
-//                   </div>
-//                   <div class="cart__item__content__settings">
-//                     <div class="cart__item__content__settings__quantity">
-//                       <p>Qté : </p>
-//                       <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-//                     </div>
-//                     <div class="cart__item__content__settings__delete">
-//                       <p class="deleteItem">Supprimer</p>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </article>
+    // Supprime le produit
+    panier.splice(productIndex, 1);
 
+    // Met à jour le nouveau panier
+    localStorage.setItem("Cart", JSON.stringify(panier));
 
+    // Réactualise les produits affichés
+    affichagePanier();
+}
 
+// Ajout quantité totale
+function calculPrixTotal() {
+  panier = JSON.parse(localStorage.getItem("Cart")) || []; // Récupération du panier
+  const prixTotal = panier.reduce((accumulator, produitsPanier) => {
+    return accumulator + (produitsPanier.price*produitsPanier.quantity);
+  }, 0);
+  const emplacementPrix = document.getElementById("totalPrice");
+  emplacementPrix.innerHTML = prixTotal;
+}
 
-
-
-
-
-// Pas de doublon 
-// */
-
-// /* Penser au total */
-
+function calculQuantiteTotal() {
+  panier = JSON.parse(localStorage.getItem("Cart")) || []; // Récupération du panier
+  const quantiteTotale = panier.reduce((accumulator, produitsPanier) => {
+    return accumulator + produitsPanier.quantity;
+  }, 0);
+  const emplacementQuantite = document.getElementById("totalQuantity")
+  emplacementQuantite.innerHTML = quantiteTotale;
+}
